0b1f6bd6c2a003e34067972e3b5c7fcc1ac67986
Ghodke <mrunal.ghodke@accenture.com>
2024-11-27 17:46:06 +0530

    connected to mongodb, created schema, implemented fetch all chat list api, fetch chat by id api

--- src/text-generate/commentedCode.ts
+++ src/text-generate/commentedCode.ts
@@ -8,3 +8,44 @@
 //     console.log(i.parts[0])
 // }
         
+// private chatHistory1 = {
+    //     chatConversation:
+    //         [
+    //             {
+    //                 role: "user",
+    //                 parts: [{ text: "hi" }],
+    //             },
+    //             {
+    //                 role: "model",
+    //                 parts: [{ text: "Great to meet you. What would you like to know?" }],
+    //             },
+    //         ]
+    // };
+
+  // async getTitleForConversation(list) {
+
+    //     console.log("hi", list);
+    //     console.log("ho");
+    //     let obj = { "_id": "6734784130aab673a9826e20", "chatId": "b4e73d67-4a04-40dc-8668-c521ab38cc5f", "userId": "someUserId", "title": "", "messages": [{ "_id": "673b6fb65b849015a47b8dd3", "role": "user", "parts": [{ "_id": "673b6fb65b849015a47b8dd4", "text": "When is Virat Kohli's birthday?" }] }, { "_id": "673b6fb65b849015a47b8dd5", "role": "model", "parts": [{ "_id": "673b6fb65b849015a47b8dd6", "text": "His birthday is on November 5." }] }, { "_id": "673b6fb65b849015a47b8dd7", "role": "user", "parts": [{ "_id": "673b6fb65b849015a47b8dd8", "text": "How old is he?" }] }, { "_id": "673b6fb65b849015a47b8dd9", "role": "model", "parts": [{ "_id": "673b6fb65b849015a47b8dda", "text": "He was born in 1988, so he turned 36 in 2024." }] }], "createdAt": "2024-11-12T14:30:00.000Z", "updatedAt": "2024-11-12T14:45:00.000Z" };
+    //     const prompt = PromptTemplate.fromTemplate(
+    //         `summarize the messages in obj in 3-4 words{obj}`
+    //     );
+
+
+    //     const formattedPrompt = await prompt.format({
+    //         obj
+    //     });
+    //     console.log(formattedPrompt, "******");
+    //     this.initializeModel();
+
+
+    //     this.chat = this.model.startChat({ history: this.chatHistory1.chatConversation, generationConfig: { maxOutputTokens: null }, });
+    //     const result = await this.chat.sendMessage(formattedPrompt);
+    //     const response = await result.response;
+
+    //     let generatedText = {
+    //         text: response.text()
+    //     }
+    //     console.log(generatedText, "generatedText");
+
+    // }
--- src/text-generate/text-generate.controller.ts
+++ src/text-generate/text-generate.controller.ts
@@ -1,43 +1,72 @@
-import { BadRequestException, Body, Controller, Get, Header, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Header, Post, Query } from '@nestjs/common';
 import { TextGenerateService } from './text-generate.service';
 import { PromptDTO } from './text-generate.dto';
-import { ApiTags, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiBody, ApiQuery } from '@nestjs/swagger';
 
 
 @Controller()
 export class TextGenerateController {
 
-  constructor(private textGenerateService: TextGenerateService) { }
+	constructor(private textGenerateService: TextGenerateService) { }
 
-  @Post('/text-generate-from-multimodal')
+	@Post('/text-generate-from-multimodal')
 	@Header('Content-Type', 'text/plain')
-  @ApiTags('generate text from text/image input from gemini api')
-  @ApiResponse({ status: 200, description: 'Data Fetched Successfully' })
-  @ApiResponse({ status: 400, description: 'Bad Request' })
-  @ApiResponse({ status: 401, description: 'Unauthorized' })
-  @ApiBody({
-    description:
-      'Generate text for chat - Payload Structure',
-    type: PromptDTO,
-    required: true,
-  })
-  async generateText(@Body() prompt: PromptDTO): Promise<any> {
-    try {
-      return await this.textGenerateService.initializeChatting(prompt);
-    } catch (error) {
-      throw new BadRequestException(error.detail);
-    }
-  }
-
-  @Get('/getChatHistory')
-	@Header('Content-Type', 'application/json')
-	@ApiTags('Get chat history')
+	@ApiTags('generate text from text/image input from gemini api')
 	@ApiResponse({ status: 200, description: 'Data Fetched Successfully' })
 	@ApiResponse({ status: 400, description: 'Bad Request' })
 	@ApiResponse({ status: 401, description: 'Unauthorized' })
-	async getDurationFilter(): Promise<any> {
+	@ApiBody({
+		description:
+			'Generate text for chat - Payload Structure',
+		type: PromptDTO,
+		required: true,
+	})
+	async generateText(@Body() prompt: PromptDTO): Promise<any> {
+		try {
+			return await this.textGenerateService.initializeChatting(prompt);
+		} catch (error) {
+			throw new BadRequestException(error.detail);
+		}
+	}
+
+	@Get('/getChatHistoryList')
+	@Header('Content-Type', 'application/json')
+	@ApiTags('Get conversation history list')
+	@ApiResponse({ status: 200, description: 'Data Fetched Successfully' })
+	@ApiResponse({ status: 400, description: 'Bad Request' })
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
+	async getChatHistoryList(): Promise<any> {
+		try {
+			return await this.textGenerateService.getChatHistoryList();
+		} catch (error) {
+			throw new BadRequestException(error.detail);
+		}
+	}
+
+	@Get('/getChatHistoryGemini')
+	@Header('Content-Type', 'application/json')
+	@ApiTags('Get chat history gemini')
+	@ApiResponse({ status: 200, description: 'Data Fetched Successfully' })
+	@ApiResponse({ status: 400, description: 'Bad Request' })
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
+	async getChatHistory(): Promise<any> {
 		try {
 			return await this.textGenerateService.getChatHistory();
+		} catch (error) {
+			throw new BadRequestException(error.detail);
+		}
+	}
+
+	@Get('/getChatByChatId')
+	@Header('Content-Type', 'application/json')
+	@ApiTags('Get chat by id')
+	@ApiResponse({ status: 200, description: 'Data Fetched Successfully' })
+	@ApiResponse({ status: 400, description: 'Bad Request' })
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
+	@ApiQuery({ name: 'chatId' })
+	async getChatById(@Query('chatId') chatId: string): Promise<any> {
+		try {
+			return await this.textGenerateService.getChatById(chatId);
 		} catch (error) {
 			throw new BadRequestException(error.detail);
 		}
