413f614baaf8a1885efa510713d6519bda139b50
Ghodke <mrunal.ghodke@accenture.com>
2025-02-16 21:23:53 +0530

    reload issue

--- src/main.ts
+++ src/main.ts
@@ -5,30 +5,44 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { APP_ROOT } from './text-generate/text-generate.constant';
 import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   dotenv.config(); // Load environment variables at the beginning
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix(APP_ROOT);
+
   const cors = require('cors');
   const corsOptions = {
-    origin: ['http://localhost:4200','https://chatify-ui.onrender.com'],
-    credentials: true,           
-    optionSuccessStatus: 200
-  }
+    origin: ['http://localhost:4200', 'https://chatify-ui.onrender.com'],
+    credentials: true,
+    optionSuccessStatus: 200,
+  };
   app.use(cors(corsOptions));
 
   app.use(express.json({ limit: '10mb' })); // Increase JSON payload limit to 10MB
   app.use(express.urlencoded({ limit: '10mb', extended: true })); // Increase URL-encoded payload limit to 10MB
+
+  // Serve Angular static files from the `dist` folder
+  app.use(express.static(path.join(__dirname, '..', 'dist', 'chatify-ui')));
+
+  // Redirect unknown routes to Angular's `index.html`
+  app.use('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'dist', 'chatify-ui', 'index.html'));
+  });
+
+  // Swagger Documentation
   const config = new DocumentBuilder()
     .setTitle('Integrate Gemini API')
     .setDescription('To generate text from Gemini API')
     .setVersion('1.0')
-    .addTag("genAI")
+    .addTag('genAI')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  await app.listen(3000);
 
+  await app.listen(3000);
 }
+
 bootstrap();
